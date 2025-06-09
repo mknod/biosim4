77bb1464ee6d875ce5b16bc685181b4a96565e97
jr <jr@rmrn.net>
2025-06-09 15:56:02 -0400

    Simplest flake to run

--- flake.nix
+++ flake.nix
@@ -1,28 +1,19 @@
 {
-  description = "Biosim4 Nix Flake";
+  description = "Minimal Biosim4 Nix Flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs }: {
-    packages = {
-      x86_64-linux = 
-        with import nixpkgs {
-          system = "x86_64-linux";
-        };
-        stdenv.mkDerivation {
-          name = "biosim4";
-          src = self;
-          installPhase = "mkdir -p $out/bin; install -t $out/bin biosim4";
-        };
-    };
-
-    apps = {
-      x86_64-linux = {
-        type = "app";
-        program = "${self.packages.x86_64-linux.default}/bin/biosim4";
+    defaultPackage.x86_64-linux = 
+      with import nixpkgs {
+        system = "x86_64-linux";
       };
-    };
+      stdenv.mkDerivation {
+        name = "biosim4";
+        src = self;
+        installPhase = "mkdir -p $out/bin; install -t $out/bin biosim4";
+      };
   };
 }
