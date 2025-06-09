fdecb4e3ae82c2286f86b3bd130d2d20c8fef5ad
jr <jr@rmrn.net>
2025-06-09 13:11:22 -0400

    added flake.nix

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "C++ development";
+
+  inputs = {
+    #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:Patryk27/nixpkgs/improve-clang-tools"
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          llvm = pkgs.llvmPackages_latest;
+          lib = nixpkgs.lib;
+
+        in
+          {
+            devShell = pkgs.mkShell {
+              nativeBuildInputs = [
+                # builder
+                # p.gnumake
+                # p.bear
+                pkgs.cmake
+                # debugger
+                llvm.lldb
+
+                # XXX: the order of include matters
+                pkgs.clang-tools
+                llvm.clang # clangd
+
+                pkgs.gtest
+              ];
+
+              buildInputs = [
+                # stdlib for cpp
+                llvm.libcxx
+              ];
+
+              # CXXFLAGS = "-std=c++17";
+              CPATH = builtins.concatStringsSep ":" [
+                (lib.makeSearchPathOutput "dev" "include" [ llvm.libcxx ])
+                (lib.makeSearchPath "resource-root/include" [ llvm.clang ])
+              ];
+            };
+          }
+    );
+}
