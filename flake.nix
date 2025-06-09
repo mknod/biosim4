57b43b1f9a51d06801b20d4a549911595f89157d
jr <jr@rmrn.net>
2025-06-09 14:41:30 -0400

    Added flake.nix

--- flake.nix
+++ flake.nix
@@ -1,49 +1,35 @@
 {
-  description = "C++ development";
+  description = "BioSim Development Shell";
 
   inputs = {
-    #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:Patryk27/nixpkgs/improve-clang-tools"
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-          llvm = pkgs.llvmPackages_latest;
-          lib = nixpkgs.lib;
+ 
+ outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        python-with-opencv = pkgs.python3.withPackages (ps: [
+          ps.opencv4
+        ]);
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            python-with-opencv
+            pkgs.pkg-config
+            pkgs.stdenv.cc.cc.lib
+            pkgs.gnuplot
+            pkgs.cmake
+            pkgs.cimg
+          ];
 
-        in
-          {
-            devShell = pkgs.mkShell {
-              nativeBuildInputs = [
-                # builder
-                # p.gnumake
-                # p.bear
-                pkgs.cmake
-                # debugger
-                llvm.lldb
-
-                # XXX: the order of include matters
-                pkgs.clang-tools
-                llvm.clang # clangd
-
-                pkgs.gtest
-              ];
-
-              buildInputs = [
-                # stdlib for cpp
-                llvm.libcxx
-              ];
-
-              # CXXFLAGS = "-std=c++17";
-              CPATH = builtins.concatStringsSep ":" [
-                (lib.makeSearchPathOutput "dev" "include" [ llvm.libcxx ])
-                (lib.makeSearchPath "resource-root/include" [ llvm.clang ])
-              ];
-            };
-          }
+          shellHook = ''
+            export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
+            echo "Python environment ready with OpenCV $(python -c 'import cv2; print(cv2.__version__)')"
+          '';
+        };
+      }
     );
 }
