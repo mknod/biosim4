a03ccdf3befd2438fa7a5642d6292370d0fa71cd
jr <jr@rmrn.net>
2025-06-09 15:54:13 -0400

    Fix derivation

--- flake.nix
+++ flake.nix
@@ -6,23 +6,23 @@
   };
 
   outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.default = 
-      with import nixpkgs {
-        system = "x86_64-linux";
-      };
-    stdenv.mkDerivation {
-        name = "biosim4";
-        src = self;
-        installPhase = "mkdir -p $out/bin; install -t $out/bin biosim4";
-      };
-    apps = {
-      default = {
-        inherit (self.packages.x86_64-linux.default) name;
-        type = "app";
-        program = "biosim4";
-      };
+    packages = {
+      x86_64-linux = 
+        with import nixpkgs {
+          system = "x86_64-linux";
+        };
+        stdenv.mkDerivation {
+          name = "biosim4";
+          src = self;
+          installPhase = "mkdir -p $out/bin; install -t $out/bin biosim4";
+        };
     };
 
+    apps = {
+      x86_64-linux = {
+        type = "app";
+        program = "${self.packages.x86_64-linux.default}/bin/biosim4";
+      };
+    };
   };
 }
