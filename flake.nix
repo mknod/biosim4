787d453edb027a25e1a7f532b3ef7d5fd3bffffa
jr <jr@rmrn.net>
2025-06-09 15:55:03 -0400

    Changed nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Biosim4 Nix Flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs }: {
