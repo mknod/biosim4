ffa21bd8d39e7946719f7e7a1aec0eaacafe340f
jr <jr@rmrn.net>
2025-06-09 16:33:16 -0400

    A simple solution

--- flake.nix
+++ flake.nix
@@ -5,7 +5,28 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }: 
+
+     let
+
+      # to work with older version of flakes
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+
+      # Generate a user-friendly version number.
+      version = builtins.substring 0 8 lastModifiedDate;
+
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
+    in
+
+    {
+
     packages = {
       default = nixpkgs.ffmpeg;
     };
@@ -36,7 +57,9 @@
           '';
         
       };
+
    
   };
+
 
 }
